86f81314d61d4800a21e61a8ee7648590b2cffaf
Amanuel Assefa <50099232+amanuel1271@users.noreply.github.com>
2021-07-12 21:03:42 +0900

    Update tc_malloc.c

--- tc_malloc.c
+++ tc_malloc.c
@@ -31,7 +31,6 @@ void tc_free(void *ptr);
 void *tc_central_init(){
     for (int j = 0;j < PAGEHEAPSIZE;j++)
         central_page_heap[j] = NULL;
-    
     pthread_spin_init(&central_lock,1);
     pthread_spin_init(&heap_lock,1);
     pthread_spin_init(&central_array_lock,1);
@@ -48,43 +47,41 @@ SPAN* span_pop(SPAN** begin){
         fprintf(stderr,"usage: pop a non NULL pointer");
         return NULL;
     }
-    
     SPAN* next = (*begin)->next;
     SPAN* cur = *begin;
-    
     if (next){
-
         next->prev = cur->prev;
         *begin = next;
         cur->next = NULL;
         cur->prev = NULL;
         return cur;
     }
-
     *begin = NULL;
     cur->next = NULL;
     cur->prev = NULL;
     return cur;
 }
 
-void span_push(SPAN** span_ptr_index,SPAN* new_span){
 
+
+
+
+
+
+void span_push(SPAN** span_ptr_index,SPAN* new_span){
     if (new_span == NULL){
         fprintf(stderr,"usage: push a non NULL pointer");
         return;
     }
-    
     if (*span_ptr_index == NULL){
         *span_ptr_index = new_span;
         new_span->next = NULL;
         new_span->prev = NULL;
         return;
     }
-    
     SPAN* next = *span_ptr_index;
     SPAN* prev = next->prev;
-    *span_ptr_index = new_span;
-    
+    *span_ptr_index = new_span;    
     new_span->next = next;
     next->prev = new_span;
     new_span->prev = prev;
@@ -95,42 +92,33 @@ void span_push(SPAN** span_ptr_index,SPAN* new_span){
 
 
 void map_id_span(size_t num_of_pages,SPAN* span_ptr,size_t starting_id){
-
     span_to_id_map* find;
-
     for(int j = 0; j < 100;j++){
-
        if (central_array[j] == NULL)
-            break;
-        
+            break;       
         find = (span_to_id_map*)central_array[j];
-
         if ((find->page_id <= starting_id) && (starting_id <= (find->page_id+ MAXPAGE - 1))){
-
             find = find + (starting_id - find->page_id);
             for(int k = 0;k < num_of_pages;k++)
                 (find + k)->span_of_page_id = span_ptr;
-
             return;
-
         }
     }
 }
 
-SPAN* lookup(size_t page_id){
-   
+
+
+
+
+SPAN* lookup(size_t page_id){   
     span_to_id_map* find;
-
     for(int j = 0; j < 100;j++){
-
         if (central_array[j] == NULL){
             break;
         }
         find = (span_to_id_map*)central_array[j];
-
         if ( (find->page_id <= page_id) && (page_id <= (find->page_id+ MAXPAGE - 1)) )
             return (find + (page_id - find->page_id))->span_of_page_id;
-
     }
     return NULL;
 }
@@ -146,31 +134,20 @@ size_t calculate_pageid(void *ptr){
 
 
 SPAN* give_span_to_central_cache_or_fetch_from_system(size_t num_of_pages){
-
     SPAN* old_span;
     void *ptr;
-
-    
-
-    if (central_page_heap[num_of_pages-1] != NULL){
-        
+    if (central_page_heap[num_of_pages-1] != NULL){      
         SPAN* x = span_pop(&(central_page_heap[num_of_pages-1]));
         return x;
     }
-
     for (size_t i = num_of_pages; i < PAGEHEAPSIZE; i++){
-
         if (central_page_heap[i] != NULL){
-
             old_span = span_pop(&(central_page_heap[i]));
             ptr = sbrk(sizeof(SPAN));
-
             if (ptr == (void*)-1){
-
                 fprintf(stderr,"The system couldn't allocate memory\n");
                 return NULL;
             }
-
             SPAN* new_span = (SPAN *)ptr;
             new_span->page_id = old_span->page_id - num_of_pages + old_span->page_size;
             new_span->page_size = num_of_pages;
@@ -182,169 +159,114 @@ SPAN* give_span_to_central_cache_or_fetch_from_system(size_t num_of_pages){
             new_span->size_of_objects = 0;
             span_push(&(central_page_heap[old_span->page_size-1]),old_span);
             map_id_span(new_span->page_size,new_span,new_span->page_id);
-
             return new_span;
-
         }
     }
-
         void *ptr2 = sbrk(MAXBLOCK);
-
         if (ptr2 == (void*)-1){
             fprintf(stderr,"The system couldn't allocate memory\n");
             fflush(stdout);
             return NULL;
 
         }
-
         void * pp = sbrk(sizeof(SPAN));
         SPAN* another_span = (SPAN*)pp;
         another_span->page_id = calculate_pageid(ptr2);
         another_span->page_size = MAXPAGE;
-        void *ptr4;
-
-        
-
+        void *ptr4;       
         for(int j = 0; j < 100;j++){
-
              if (central_array[j] == NULL){
                  ptr4 = sbrk(MAXPAGE * sizeof(span_to_id_map));
                  span_to_id_map* find = (span_to_id_map*)ptr4;
                  central_array[j] = ptr4;
-
                  for (int k = 0; k < MAXPAGE;k++)
                      (find + k)->page_id = another_span->page_id + k;
-
-
                  break;
              }
         }
-
          SPAN* large_span;
          void* ptr3;
          char* an_ptr = (char *)ptr2;
-
-         for (int k = 0;  k < 512;  k++){
-                
+         for (int k = 0;  k < 512;  k++){                
                 ptr3 = sbrk(sizeof(SPAN));
-
                 if (ptr3 == (void*)-1){
-
                     fprintf(stderr,"The system couldn't allocate memory\n");
                     fflush(stdout);
                     return NULL;
-
                 }
-
                 large_span = (SPAN *)ptr3;
                 large_span->page_id = another_span->page_id + (k * 256);
                 large_span->page_size = PAGEHEAPSIZE;
                 large_span->size_of_objects = 0;
                 large_span->obj_ptr = (void *)(an_ptr + (k * 256 * PAGESIZE));
-
                 span_push(&(central_page_heap[PAGEHEAPSIZE -1]),large_span);
                 map_id_span(large_span->page_size,large_span,large_span->page_id);
-
         }
         return give_span_to_central_cache_or_fetch_from_system(num_of_pages);
 
 }
 
 void Free_Span_to_Central_Page_heap(SPAN ** span){
-    // look up the prev page's span
-
-   
+    // look up the prev page's span   
     pthread_spin_lock(&heap_lock);
     size_t comb_page_size;
     pthread_spin_lock(&central_array_lock);
-
     SPAN* prev_span = lookup( ((*span)->page_id) - 1);
     pthread_spin_unlock(&central_array_lock);
-
     if (prev_span == *span){
         pthread_spin_unlock(&heap_lock);
         return;
     }
-
     SPAN** find;
     SPAN *search_for_span,*ptr;
-
-    if (prev_span != NULL && prev_span->size_of_objects == 0 && prev_span->obj_ptr){
-    
+    if (prev_span != NULL && prev_span->size_of_objects == 0 && prev_span->obj_ptr){    
         comb_page_size = prev_span->page_size + (*span)->page_size;
-
         if ( !prev_span->num_of_objects_taken && (comb_page_size <= PAGEHEAPSIZE) ){
-
             find = central_page_heap + prev_span->page_size -1;
             search_for_span = *find;
             ptr = search_for_span;
-
-
-                if (ptr != NULL){
-                    
+                if (ptr != NULL){                  
                     if (prev_span == ptr){ //If the span is the first span
                         if (*find)
                             span_pop(find);
                     }
-
                     else{
-
                         while(ptr != NULL){
-                            if (ptr == prev_span){
-                             
-                                if (ptr->prev->next){
-
+                            if (ptr == prev_span){                             
+                               if (ptr->prev->next){
                                     span_pop(&(ptr->prev->next));
                                     break;
                                 }
-
                             }
                             ptr = ptr->next;
-
                         }
                     }
-
                     prev_span->page_size += (*span)->page_size;
                     *span = prev_span; // I passed in a double pointer to now change the span pointer given in the argument for merge
-
                 }
-
          }
     }
 
     pthread_spin_lock(&central_array_lock);
     SPAN* next_span = lookup((*span)->page_id + (*span)->page_size);
     pthread_spin_unlock(&central_array_lock);
-
     if (next_span == *span){
-
         pthread_spin_unlock(&heap_lock);
         return;
     }
-
      if (next_span != NULL && next_span->size_of_objects == 0) {
         comb_page_size = next_span->page_size + (*span)->page_size;
-
-
         if ( !next_span->num_of_objects_taken && (comb_page_size <= PAGEHEAPSIZE) && next_span->obj_ptr){
-
             find = central_page_heap + next_span->page_size -1;
             search_for_span = *find;
             ptr = search_for_span;
-
-            if (ptr != NULL){
-
-                
-                    if (next_span == ptr){ //If the span is the first span
-                        
+            if (ptr != NULL){                
+                    if (next_span == ptr){ //If the span is the first span                        
                         if (*find)
                             span_pop(find);
                     }
-
-                    else{
-                        
-                        while(ptr != NULL){
-                            
+                    else{                        
+                        while(ptr != NULL){                            
                             if (ptr == next_span){
                                 if (ptr->prev->next){
                                     span_pop(&(ptr->prev->next));
@@ -353,18 +275,13 @@ void Free_Span_to_Central_Page_heap(SPAN ** span){
                             }
                             ptr = ptr->next;
                         }
-                    }
-                
+                    }                
                     if (ptr != NULL)
                         (*span)->page_size += next_span->page_size;//Merged
             }
         }
-
-     }
-    
+     }    
     pthread_spin_unlock(&heap_lock);
-
-
 }
 
 
@@ -372,60 +289,48 @@ void Free_Span_to_Central_Page_heap(SPAN ** span){
 
 
 size_t index_of_central_free_list(size_t block_size){
-    //make sure that a tc_malloc(0) call will return a NULL pointer
-
     if ( (block_size > 8 * PAGESIZE) || (block_size == 0)){
 
         fprintf(stderr,"usage: 0 <= blocksize <= 32K:  block size given is not indexable\n");
         return -100;
-    }
-    
+    }    
     size_t save;
-
     if (block_size <= 64){
         save = (64 - (int)block_size)/8;
         return 7 - save;
-    }
-    
+    }    
     else if (block_size <= 2048){
         save = (2048 - (int)block_size)/64;
         return (30 - save) + 8;
-    }
-    
+    }    
     else{
         save = (32768 - (int)block_size)/256;
         return (119 - save) + 39;
 
     }
-
 }
 
+
+
+
 size_t move_size(size_t size){
-
     if (!size)
-        return 1;
-    
+        return 1;    
     int num_elem = (int)(MAXSMALLOBJ/size);
-
     if (num_elem < 3)
         num_elem = 3;
-
     else if (num_elem > 576)
         num_elem = 576;
-
     return num_elem;
 }
 
 
 
-size_t obj_size_to_page(size_t size){
-    
+size_t obj_size_to_page(size_t size){   
     size_t num_elem = move_size(size);
     size_t num_of_pages = (num_elem * size)/PAGESIZE;
-
     if (num_of_pages == 0)
         num_of_pages = 1;
-
     return num_of_pages;
 }
 
